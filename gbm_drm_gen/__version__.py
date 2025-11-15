6da72dec55960fb8bcb849c81284f1d08622ec55
Xinxiang Sun <sunxx@nao.cas.cn>
2025-11-15 17:30:08 +0800

    修改：     gbm_drm_gen/__version__.py

--- gbm_drm_gen/__version__.py
+++ gbm_drm_gen/__version__.py
@@ -1,8 +1,14 @@
+'''
+Date: 2025-08-15 15:34:36
+LastEditors: Xinxiang Sun sunxx@nao.cas.cn
+LastEditTime: 2025-10-29 11:37:11
+FilePath: /research/software/gbm_drm_gen/gbm_drm_gen/__version__.py
+'''
 # 8b    d8 Yb  dP 88""Yb    db     dP""b8 88  dP    db     dP""b8 888888
 # 88b  d88  YbdP  88__dP   dPYb   dP   `" 88odP    dPYb   dP   `" 88__
 # 88YbdP88   8P   88"""   dP__Yb  Yb      88"Yb   dP__Yb  Yb  "88 88""
 # 88 YY 88  dP    88     dP""""Yb  YboodP 88  Yb dP""""Yb  YboodP 888888
 
-VERSION = (0, 2, 1)
+VERSION = (0, 2, 2)
 
 __version__ = ".".join(map(str, VERSION))
